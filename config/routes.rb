8de5b847cdc330f305fdf38087f53b2cfc8e97f7
Sergio Bayona <bayona.sergio@gmail.com>
2025-09-17 10:33:05 -0500

    jobs ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # config/routes.rb
 Rails.application.routes.draw do
+  mount MissionControl::Jobs::Engine, at: "/jobs"
+
   get "/ingest", to: "webhooks#verify" # GET /ingest
   post "/ingest", to: "webhooks#create" # POST /ingest
 
